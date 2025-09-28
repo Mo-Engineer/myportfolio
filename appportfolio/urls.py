9b73d602bf4c724a94738cc6b3fb7a7fa950d0d3
Mo-Engineer <mohamedinyangalio@gmail.com>
2025-09-29 02:58:22 +0300

    Merge unrelated histories

--- appportfolio/urls.py
+++ appportfolio/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 from .views import *
+from django.conf import settings
 
 urlpatterns = [
     path('', homeView, name='home'),
+    re_path(r'^sitemap\.xml$', serve, {'path': 'sitemap.xml', 'document_root': settings.BASE_DIR}),
 ]
